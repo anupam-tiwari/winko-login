965674c6d13f256176944bf705a7dd1cd3671093
anupam-tiwari <appleanupam@gmail.com>
2020-06-16 02:31:25 -0700

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_XMt0TLyvj', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1041blj5pl1pd3sj0dr0shvdii', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://cilbbexe52.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
